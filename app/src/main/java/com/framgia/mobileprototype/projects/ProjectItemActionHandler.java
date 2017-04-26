69af14e02edd9d207cdc21c598a0c0b57456cb44
tuannt <tuanbaros@yahoo.com.vn>
2017-04-26 16:39:57 +0700

    task #2 change project option to menu

--- app/src/main/java/com/framgia/mobileprototype/projects/ProjectItemActionHandler.java
+++ app/src/main/java/com/framgia/mobileprototype/projects/ProjectItemActionHandler.java
@@ -1,5 +1,11 @@
 package com.framgia.mobileprototype.projects;
 
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.framgia.mobileprototype.R;
 import com.framgia.mobileprototype.data.model.Project;
 
 /**
@@ -24,5 +30,28 @@ public class ProjectItemActionHandler {
 
     public void openDetailProject(Project project) {
         mListener.openDetailProject(project);
+    }
+
+    public void openProjectOption(View view, final int position, final Project project) {
+        PopupMenu popupMenu = new PopupMenu(view.getContext(), view);
+        MenuInflater menuInflater = popupMenu.getMenuInflater();
+        menuInflater.inflate(R.menu.activity_projects_option, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_edit:
+                        editProject(project);
+                        break;
+                    case R.id.action_share:
+                        break;
+                    case R.id.action_remove:
+                        removeProject(project, position);
+                        break;
+                }
+                return true;
+            }
+        });
+        popupMenu.show();
     }
 }
